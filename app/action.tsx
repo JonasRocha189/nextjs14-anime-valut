9fab1b300e935c8e8b4c9e54c9c3d1e7dad055dd
JonasRocha189 <jonas.rocha189@gmail.com>
2024-01-15 23:29:39 -0300

    deploy project

---
+++ app/action.tsx
@@ -0,0 +1,15 @@
+"use server"
+
+import AnimeCard, { AnimeProp } from "@/components/AnimeCard"
+
+export const fetchAnimne = async (page: number) => {
+  const resonse = await fetch(`https://shikimori.one/api/animes?page=${page}&limit=8&order=popularity`)
+
+  const data = await resonse.json()
+
+  console.log(data)
+
+  return data.map((item: AnimeProp, index: number) => (
+    <AnimeCard key={item.id} anime={item} index={index} />
+  ))
+}
--- components/LoadMore.tsx
+++ components/LoadMore.tsx
@@ -1,10 +1,36 @@
-import Image from "next/image";
+'use client'
+
+import Image from 'next/image'
+import { useInView } from 'react-intersection-observer'
+import { useEffect, useState } from 'react'
+import { fetchAnimne } from '@/app/action'
+import AnimeCard, { AnimeProp } from './AnimeCard'
+
+let page = 2
+
+export type AnimeCard = JSX.Element
 
 function LoadMore() {
+  const { ref, inView } = useInView()
+  const [data, setData] = useState<AnimeProp[]>([])
+
+  useEffect(() => {
+    if (inView) {
+      fetchAnimne(page).then((res) => {
+        setData([...data, ...res])
+        page++
+      })
+    }
+  }, [inView, data])
+
   return (
     <>
+      <section className="grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-10">
+        {data}
+      </section>
+
       <section className="flex justify-center items-center w-full">
-        <div>
+        <div ref={ref}>
           <Image
             src="./spinner.svg"
             alt="spinner"
@@ -15,7 +41,7 @@ function LoadMore() {
         </div>
       </section>
     </>
-  );
+  )
 }
 
-export default LoadMore;
+export default LoadMore
